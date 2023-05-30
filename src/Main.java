9b7f6f83544c66cdd929687185c4049410cbbb5b
Dev-IgorLopes <igorlopes.adm1@gmail.com>
2023-05-29 22:27:43 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.BootCamp;
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-      Curso curso = new Curso("Java", "Curso Java", 120);
+      Curso curso = new Curso();
+      curso.setTitulo("Java");
+      curso.setDescricao("Curso Java");
+      curso.setCargaHoraria(120);
+      Conteudo conteudo = new Curso();
       
 
+      Mentoria mentoria = new Mentoria();
+      mentoria.setTitulo("Aulão Kotlin");
+      mentoria.setDescricao("Curso Kotlin");
+      mentoria.setData(LocalDate.now());
 
-      Mentoria mentoria = new Mentoria("Aulão Kotlin", "Curso Kotlin", LocalDate.now());
+      BootCamp bootcamp1 = new BootCamp();
+      bootcamp1.setNome("BootCamp Kotlin Developer BackEnd");
+      bootcamp1.setDescricao("Backend Developer é um programa gratuito para formação de profissionais que estão iniciando sua carreira e buscam mais conhecimento técnico em desenvolvimento back-end. ");
+      bootcamp1.getConteudos().add(curso);
+      bootcamp1.getConteudos().add(mentoria);
 
+      Dev devs = new Dev();
+      devs.setNome("Igor");
+      devs.inscreverBootCamp(bootcamp1);
+      System.out.println("Conteudos inscritos: " + devs.getConteudosInscritos());
+      devs.progredir();
+      System.out.println("Conteudos inscritos: " + devs.getConteudosInscritos());
+    
+      System.out.println("Conteudos Concluidos: " + devs.getConteudosConcluidos());
+      System.out.println("XP"+ devs.calcularTotalXp());
 
-      System.out.println(curso);
-      System.out.println(mentoria);
     }
 }
